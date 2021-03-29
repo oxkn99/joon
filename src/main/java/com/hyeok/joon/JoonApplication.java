b0b380da442e47b2bb107354f7f493b2ad99db32
joonhyeok97 <oxkn77@naver.com>
2021-03-29 09:51:19 +0900

    2021-03-29 init

---
+++ src/main/java/com/hyeok/joon/JoonApplication.java
@@ -0,0 +1,13 @@
+package com.hyeok.joon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeok/joon/JoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeok.joon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
