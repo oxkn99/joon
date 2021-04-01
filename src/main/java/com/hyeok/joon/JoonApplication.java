3308facfd16120cf405d72321e686456e4275644
Hyeok <oxkn88@gmail.com>
2021-04-01 10:51:00 +0900

    build war change

--- src/main/java/com/hyeok/joon/JoonApplication.java
+++ src/main/java/com/hyeok/joon/JoonApplication.java
@@ -2,9 +2,16 @@ package com.hyeok.joon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JoonApplication {
+public class JoonApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JoonApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoonApplication.class, args);
